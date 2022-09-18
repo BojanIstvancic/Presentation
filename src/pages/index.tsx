87549b6a1cea5b435f6883f6017621d6fa75c8d1
BojanIstvancic <bojan.istvancic@gmail.com>
2022-09-18 17:53:16 +0200

    home competed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,22 +6,26 @@ import styled from "styled-components";
 import Container from "../components/Container";
 import Image from "next/image";
 import HomeImage from "public/images/pages/index/home-image.png";
+import ReactTypingEffect from "react-typing-effect";
 
 const StyledMainContent = styled.main``;
 
+// ---- HOME ----
 const Home = styled.section``;
 const HomeInner = styled.div`
   padding: 40px 0;
-`;
-const HomeTitle = styled.h1``;
-const HomeContent = styled.div`
+
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
+
+  @media (min-width: 800px) {
+    flex-direction: row-reverse;
+    align-items: flex-start;
+  }
 `;
 const HomeButton = styled.a`
-  margin-top: 15px;
+  margin-top: 24px;
   padding: 12px 25px;
   background: var(--blue);
   font-weight: bold;
@@ -34,6 +38,96 @@ const HomeButton = styled.a`
   &:hover {
     background: var(--dark-blue);
   }
+
+  @media (min-width: 800px) {
+    border-radius: 8px;
+  }
+`;
+const HomeTitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  flex: 1;
+
+  ${HomeButton} {
+    margin-top: 40px;
+
+    @media (max-width: 799px) {
+      display: none;
+    }
+  }
+`;
+const HomeTitle = styled.h1`
+  margin-bottom: 40px;
+  font-size: 20px;
+  width: 240px;
+
+  @media (min-width: 450px) {
+    width: 400px;
+  }
+
+  @media (min-width: 800px) {
+    margin-bottom: 0;
+    font-size: 26px;
+    width: unset;
+    flex: 1;
+  }
+
+  @media (min-width: 1100px) {
+    margin-top: 20px;
+    font-size: 35px;
+  }
+
+  span {
+    display: block;
+    font-size: 1em;
+
+    @media (min-width: 450px) {
+      &.i-am,
+      &.position {
+        font-size: 1.5em;
+      }
+    }
+
+    &.position {
+      font-weight: bold;
+      span {
+      }
+      color: var(--blue);
+    }
+  }
+`;
+const HomeContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media (min-width: 800px) {
+    margin-right: 40px;
+    justify-content: flex-start;
+    align-items: flex-start;
+
+    ${HomeButton} {
+      display: none;
+    }
+  }
+`;
+const HomeImageContainer = styled.div`
+  position: relative;
+  height: 235px;
+  width: 235px;
+
+  @media (min-width: 800px) {
+    height: 250px;
+    width: 250px;
+  }
+
+  @media (min-width: 1000px) {
+    height: 400px;
+    width: 400px;
+  }
 `;
 
 interface HomePageProps {}
@@ -41,21 +135,41 @@ interface HomePageProps {}
 const HomePage: React.FC<HomePageProps> = ({}) => {
   const translation = useTranslations("home");
 
+  const position = translation("home-title-label-4");
+
   return (
     <Layout>
       <StyledMainContent>
         <Home>
           <Container>
             <HomeInner>
-              <HomeTitle>{translation("text")}</HomeTitle>
+              <HomeTitleContainer id="home">
+                <HomeTitle>
+                  <span>{translation("home-title-label-1")}</span>
+                  <span className="i-am">
+                    {translation("home-title-label-2")}
+                  </span>
+                  <span>{translation("home-title-label-3")}</span>
+                  <span className="position">
+                    <ReactTypingEffect
+                      text={[position]}
+                      eraseDelay={6000000}
+                      typingDelay={250}
+                    />
+                  </span>
+                </HomeTitle>
+                <HomeButton
+                  href="/index/CV.pdf"
+                  download="Bojan-Istvancic-CV"
+                  target="_blank"
+                >
+                  {translation("home-download")}
+                </HomeButton>
+              </HomeTitleContainer>
               <HomeContent>
-                <Image
-                  src={HomeImage}
-                  alt="home-image"
-                  width="290"
-                  height="290"
-                  style={{ borderRadius: "0% 28%" }}
-                />
+                <HomeImageContainer>
+                  <Image src={HomeImage} alt="home-image" layout="fill" />
+                </HomeImageContainer>
                 <HomeButton
                   href="/index/CV.pdf"
                   download="Bojan-Istvancic-CV"
