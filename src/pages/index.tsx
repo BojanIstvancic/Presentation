db695f9952abaab52dc2e4c0f363df9ad645c31a
BojanIstvancic <bojan.istvancic@gmail.com>
2022-11-04 09:14:38 +0100

    adjust react color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -332,7 +332,7 @@ const Technology = styled.span`
     background-color: var(--darker-blue);
   }
   &.react {
-    background-color: #0cbcdc;
+    background-color: #0db6d4;
   }
   &.mui {
     background-color: #0079f2;
